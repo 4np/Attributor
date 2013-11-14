95226eb47b55925d3b71f33c15baa331c5d2e5f4
Jeroen Wesbeek <work@osx.eu>
2013-11-14 23:46:22 +0100

    Lecture 5 (View Controller Lifecycle): added changeBodySelectionColorToMatchBackgroundOfButton

--- Attributor/AttributorViewController.m
+++ Attributor/AttributorViewController.m
@@ -9,10 +9,17 @@
 #import "AttributorViewController.h"
 
 @interface AttributorViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UILabel *headline;
 @end
 
 @implementation AttributorViewController
+
+- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range:self.body.selectedRange];
+}
 
 - (void)viewDidLoad
 {
@@ -20,10 +27,10 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+//- (void)didReceiveMemoryWarning
+//{
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
 
 @end
